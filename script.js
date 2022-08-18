4432a5475cfe7561560aea8430ff8fb755cae218
totalSophie <32367816+totalSophie@users.noreply.github.com>
2022-08-18 16:14:38 +0300

    Add the files for webcam access

---
+++ script.js
@@ -0,0 +1,40 @@
+var video = document.querySelector("#videoElement");
+var stopVideo = document.querySelector("#stop");
+var canvas, ctx;
+
+
+if (navigator.mediaDevices.getUserMedia) {
+navigator.mediaDevices.getUserMedia({ video: true,})
+    .then(function (stream) {
+    video.srcObject = stream;
+    })
+    .catch(function (error) {
+    console.log("Something went wrong!");
+    });
+}
+
+    stopVideo.addEventListener("click", stop, false);
+
+function stop(e) {
+    var stream = video.srcObject;
+    var tracks = stream.getTracks();
+
+    for (var i = 0; i < tracks.length; i++) {
+    var track = tracks[i];
+    track.stop();
+    }
+
+    video.srcObject = null;
+}
+
+function init() {
+    // Get the canvas and obtain a context for
+    // drawing in it
+    canvas = document.getElementById("myCanvas");
+    ctx = canvas.getContext('2d');
+  }
+
+function snapshot() {
+   // Draws current image from the video element into the canvas
+  ctx.drawImage(video, 0,0, canvas.width, canvas.height);
+}
